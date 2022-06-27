507d43fbb6802249b8cb6d23697454920e686f15
rameshmane7218 <rameshmane7218@gmail.com>
2022-06-27 12:53:16 +0530

    all done

--- unit-5/sprint-1/evaluation/RCT-201-E1/index.js
+++ unit-5/sprint-1/evaluation/RCT-201-E1/index.js
@@ -28,7 +28,7 @@ app.post("/getmeip", (req, res) => {
 //Make an API using Express to perform CRUD Operation
 
 app.post("/products/create", (req, res) => {
-  postData = req.body;
+  let postData = req.body;
   console.log(postData);
   postData = { ...postData, createdAt: new Date().toJSON() };
   fs.readFile("./products.json", { encoding: "utf-8" }, (err, data) => {
@@ -67,12 +67,50 @@ app.get("/products", (req, res) => {
 });
 
 app.put("/products/:productId", (req, res) => {
-  res.send("This is put request");
+  const { productId } = req.params;
+  let updateData = req.body;
+  // console.log(productId);
+
+  fs.readFile("./products.json", { encoding: "utf-8" }, (err, data) => {
+    if (err) {
+      console.log("Something went wrong while reading data");
+    }
+    const parsedProducts = JSON.parse(data);
+    const updateProduct = parsedProducts.products.filter(
+      (el) => el.id == productId
+    );
+    for (let key in updateProduct[0]) {
+      if (key == "id" || key == "createdAt") {
+        updateProduct[0][key] = updateProduct[0][key];
+      } else {
+        updateProduct[0][key] = updateData[key];
+      }
+    }
+    // console.log("updated product: ",updateProduct);
+    parsedProducts.products = [
+      ...parsedProducts.products,
+      updateProduct[0],
+    ].sort((a, b) => {
+      if (a.createdAt > b.createdAt) return 1;
+      if (a.createdAt < b.createdAt) return -1;
+    });
+
+    fs.writeFile(
+      "./products.json",
+      JSON.stringify(parsedProducts),
+      (err, data) => {
+        if (err) {
+          console.log("Error occurred while writing data");
+        }
+        res.send(updateProduct[0]);
+      }
+    );
+  });
 });
 
 app.delete("/products/:productId", (req, res) => {
   const { productId } = req.params;
-  console.log(productId);
+  //   console.log(productId);
 
   fs.readFile("./products.json", { encoding: "utf-8" }, (err, data) => {
     if (err) {
